b0ac6dfb2086b3cb8d926d54271559dc11c91aa7
treasureBear94 <qvo7896@gmail.com>
2020-09-27 23:42:01 +0900

    3-3. 구조변경, mustache include

--- src/main/java/com/youtube/myslipp/web/UserController.java
+++ src/main/java/com/youtube/myslipp/web/UserController.java
@@ -7,23 +7,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
 
-    @PostMapping("/create")
+    @PostMapping("")
     public String create(User user) {
         System.out.println("user = " + user);
         userRepository.save(user);
-        return "redirect:/list";
+        return "redirect:/users";
     }
 
-    @GetMapping("/list")
+    @GetMapping("")
     public String list(Model model) {
         model.addAttribute("users", userRepository.findAll());
-        return "list";
+        return "/user/list";
     }
 }
