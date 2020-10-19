34f8660835a2711513b299fe1f140d550b25eee7
treasureBear94 <qvo7896@gmail.com>
2020-10-19 23:27:58 +0900

    4-5. 질문 목록 기능

--- src/main/java/com/youtube/myslipp/web/HomeController.java
+++ src/main/java/com/youtube/myslipp/web/HomeController.java
@@ -1,13 +1,23 @@
 package com.youtube.myslipp.web;
 
+import com.youtube.myslipp.domain.QuestionRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    private final QuestionRepository questionRepository;
+
+    public HomeController(QuestionRepository questionRepository) {
+        this.questionRepository = questionRepository;
+    }
+
     @GetMapping("")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("questions", questionRepository.findAll());
+
         return "index";
     }
 }
