21e5767039b51e2e3670df61a3b30cf006b3c7cc
treasureBear94 <qvo7896@gmail.com>
2020-11-01 23:32:21 +0900

    5-2. 질문 상세보기

--- src/main/java/com/youtube/myslipp/web/QuestionController.java
+++ src/main/java/com/youtube/myslipp/web/QuestionController.java
@@ -4,7 +4,9 @@ import com.youtube.myslipp.domain.Question;
 import com.youtube.myslipp.domain.QuestionRepository;
 import com.youtube.myslipp.domain.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -38,6 +40,12 @@ public class QuestionController {
         questionRepository.save(newQuestion);
 
         return "redirect:/";
+    }
+    
+    @GetMapping("/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        model.addAttribute("question", questionRepository.findById(id).orElseThrow());
+        return "qna/show";
     }
 
 }
